520d84e788778f68c6d7ec7568e6794ee2a8b5b6
Jean <mendes.jean.cs@gmail.com>
2023-01-04 08:47:20 -0300

    listen app with PORT from .env

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 connectToDB((err) => {
   if (!err) {
-    app.listen(5000, () => {
+    app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
   }
