cec9cbc0f36ef2f63c0068162e4c5fed94b01afd
Jean <mendes.jean.cs@gmail.com>
2023-01-09 09:51:54 -0300

    add VehiclesDescription

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import Routes from "./routes";
 
-import "./styles.scss";
+import "./styles/styles.scss";
 
 function App() {
   return <Routes />;
--- client/src/components/VehicleDescription/index.tsx
+++ client/src/components/VehicleDescription/index.tsx
@@ -1,8 +1,9 @@
-import { Button } from "antd";
+import { Button, Col, Divider, Row } from "antd";
 import { ReactSVG } from "react-svg";
 
 import TagIcon from "../../assets/icons/tag.svg";
 import { IVehicle } from "../../types/interfaces";
+import EditIcon from "../../assets/icons/edit.svg";
 
 interface IVehicleDescription {
   vehicle: IVehicle | undefined;
@@ -12,23 +13,32 @@ function VehicleDescription({ vehicle }: IVehicleDescription) {
   if (!vehicle) return null;
 
   return (
-    <div className="description">
-      <div>
-        <h3>{vehicle.vehicle}</h3>
-        <div>
-          <div>
-            <h4>Brand</h4>
-            <h4>{vehicle.brand}</h4>
-          </div>
-          <div>
+    <div className="descriptions">
+      <div className="descriptions__info">
+        <h3 className="descriptions__vehicle">{vehicle.vehicle}</h3>
+        <Row className="descriptions__brand-wrapper">
+          <Col span={12}>
+            <h4 className="descriptions__brand-title">Brand</h4>
+            <h4 className="descriptions__brand">{vehicle.brand}</h4>
+          </Col>
+          <Col span={12}>
             <h4>Year</h4>
             <h4>{vehicle.year}</h4>
-          </div>
-          <p>{vehicle.description}</p>
-        </div>
+          </Col>
+        </Row>
+        <p>{vehicle.description}</p>
       </div>
-      <div>
-        <Button>EDIT</Button>
+      <Divider orientation="center" />
+      <div className="descriptions__actions">
+        <Button
+          icon={
+            <ReactSVG className="descriptions__actions__icon" src={EditIcon} />
+          }
+          size="large"
+          className="descriptions__actions__button"
+        >
+          EDIT
+        </Button>
         <ReactSVG src={TagIcon} alt="Tag Icon" />
       </div>
     </div>
